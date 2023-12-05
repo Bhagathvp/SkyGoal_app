e573a1b4a65b11e622d5ca61524f373c757dfe10
Bhagath Vaishnav P <bhagathvp.ramu@gmail.com>
2023-12-06 01:43:13 +0530

    Production url added

--- frontend/src/constants/axios.js
+++ frontend/src/constants/axios.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL=process.env.NODE_ENV==='production' ? process.env.REACT_APP_SERVER_URL:process.env.REACT_APP_LOCAL_SERVER_URL;
+const URL=process.env.NODE_ENV==='production' ? process.env.REACT_APP_PROD_SERVER_URL:process.env.REACT_APP_LOCAL_SERVER_URL;
 console.log(URL)
 
 const instance = axios.create({
